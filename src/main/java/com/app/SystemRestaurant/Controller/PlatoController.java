4ea8321c33e7b9612210abce3a22fd4e68da60a6
Chriso <huamancruzcristianjesus@gmail.com>
2025-10-24 23:42:36 -0500

    Small update

--- src/main/java/com/app/SystemRestaurant/Controller/PlatoController.java
+++ src/main/java/com/app/SystemRestaurant/Controller/PlatoController.java
@@ -1,7 +1,6 @@
 package com.app.SystemRestaurant.Controller;
 import com.app.SystemRestaurant.Model.ClasesGestion.Plato;
 import com.app.SystemRestaurant.Repository.PlatoRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
