9c7659ea5e2539692232abc9f509b86d8a8444a6
JuFer007 <juniorfernandozumaetagolac@gmail.com>
2025-12-12 08:22:38 -0500

    cambios rneder

--- src/main/java/com/app/SystemRestaurant/SystemRestaurantApplication.java
+++ src/main/java/com/app/SystemRestaurant/SystemRestaurantApplication.java
@@ -16,17 +16,4 @@ public class SystemRestaurantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SystemRestaurantApplication.class, args);
 	}
-
-    @PostConstruct
-    public void iniciarServidorNode() {
-        try {
-            File serverNodeJs = new File("serverNode/server.js");
-            ProcessBuilder pb = new ProcessBuilder("node", serverNodeJs.getAbsolutePath());
-            pb.inheritIO();
-            pb.start();
-            System.out.println("Servidor Node iniciado automáticamente.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
